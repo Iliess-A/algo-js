5b3760970a071f3e38cf15c604a783f8f2131074
Iliess <m.iliess.abdelmadjid@gmail.com>
2024-03-29 11:09:28 +0100

    managing file new folder partOne

---
+++ partOne/01.intro/1.2/app.js
@@ -0,0 +1,7 @@
+const name='Abdelmadjid';
+const firstname='Iliess';
+const city='Couillet';
+
+console.log(name);
+console.log(firstname);
+console.log(city);
---
+++ partOne/01.intro/1.3/app.js
@@ -0,0 +1,8 @@
+const name='Abdelmadjid';
+const firstname='Iliess';
+const city='Couillet';
+
+console.log(`Your name is ${name} ${firstname} and you live in ${city}.`);
+(() => {
+    console.log(`Your name is ${name} ${firstname} and you live in ${city}.`);
+})();
