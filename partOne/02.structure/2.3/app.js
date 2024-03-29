c73fba361ccf4a9b5b510f68188742e14d8b703b
Iliess <m.iliess.abdelmadjid@gmail.com>
2024-03-29 11:32:40 +0100

    ajout des dossier pour les exo de structure

---
+++ partOne/02.structure/2.3/app.js
@@ -0,0 +1,13 @@
+let nb = 5;
+ nb+=3;
+ console.log("5 +3 vaux " + nb + "!");
+
+ nb -= 5;
+ console.log("la reponse -5 vaux " + nb + "!");
+
+ nb*=3;
+ console.log("la reponse *3 vaux " + nb + "!");
+ nb/=2;
+ console.log("la reponse /2 vaux " + nb + "!");
+ nb%2;
+ console.log("la reponse modulo 2 vaux " + nb + "!");
