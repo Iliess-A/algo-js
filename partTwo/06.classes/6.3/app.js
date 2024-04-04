ff8a4333e52113a732e6f9b70e8879ded3f1d37f
iliess <m.iliess.abdelmadjid@gmail.com>
2024-04-04 10:52:25 +0200

    fin exos sur les classes

--- partTwo/06.classes/6.3/app.js
+++ partTwo/06.classes/6.3/app.js
@@ -13,23 +13,44 @@
         collides(otherRectangle){
             
             //formule qui regarde si les surface se rencontre
-            if (this.topLeftXpos<otherRectangle.topLeftXpos+otherRectangle.width && this.topLeftXpos + this.width>otherRectangle.topLeftXpos && this.topLeftYpos<otherRectangle.topLeftYpos+otherRectangle.length && this.topLeftYpos+this.length>otherRectangle.topLeftYpos) return true ;
+            if (this.topLeftXpos < otherRectangle.topLeftXpos + otherRectangle.width &&
+                this.topLeftXpos + this.width > otherRectangle.topLeftXpos && 
+                this.topLeftYpos < otherRectangle.topLeftYpos + otherRectangle.length &&
+                this.topLeftYpos + this.length > otherRectangle.topLeftYpos) return true ; return false ;
         }
      }
 
-     let allRectangle=[];
+    let thousoundRec =()=>{
 
-
-
-     let randomR=()=>{
-       return Math.floor(Math.random());
-     }
+        let allRectangle=[];
 
      for(i=0;i<=1000;i++){
+        
+        let Xpos= Math.floor(Math.random()*1000);
+        let Ypos= Math.floor(Math.random()*1000);
+        let width= Math.floor(Math.random()*10);
+        let length= Math.floor(Math.random()*10);
 
-        allRectangle.push(new Rectangle[randomR,randomR,randomR,randomR]);
+        allRectangle.push(new Rectangle(Xpos,Ypos,width,length));
 
+        }
+
+     return allRectangle
      }
-    
-     console.log(allRectangle());
+
+
+     let mes_rect=thousoundRec();
+     //console.log(mes_rect);
+     let cpt=0;
+
+     for (i=0;i<mes_rect.length;i++){
+
+        for(j=i+1 ; j<mes_rect.length ; j++){
+
+            if (mes_rect[i].collides(mes_rect[j])) cpt++;
+        }
+     }
+
+    console.log(`il y'as eu ${cpt} collisions`)
+
 })();
